eac773c67e3b151099d589fa76b7ab48d9baa16e
Pereira277 <lucaspereira2779@gmail.com>
2026-03-22 18:52:20 -0300

    Adiciona ingress unico e aumenta node group para suportar ingress-nginx

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -271,9 +271,9 @@ resource "aws_eks_node_group" "main" {
   subnet_ids      = [aws_subnet.private_a.id, aws_subnet.private_b.id]
 
   scaling_config {
-    desired_size = 1
-    min_size     = 1
-    max_size     = 1
+    desired_size = 2
+    min_size     = 2
+    max_size     = 2
   }
 
   instance_types = [var.node_instance_type]
